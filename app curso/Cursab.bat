38fa0db274d307388c7be676a88231dacb8b060b
earp519 <earp519@hotmail.com>
2017-04-02 22:13:44 -0500

    final de app pepe
    se agrego un calendario y se tiene todas las plantillas en estatico

---
+++ app curso/Cursab.bat
@@ -0,0 +1,18 @@
+@ECHO OFF
+REM  QBFC Project Options Begin
+REM  HasVersionInfo: No
+REM  Companyname: 
+REM  Productname: 
+REM  Filedescription: 
+REM  Copyrights: 
+REM  Trademarks: 
+REM  Originalname: 
+REM  Comments: 
+REM  Productversion:  0. 0. 0. 0
+REM  Fileversion:  0. 0. 0. 0
+REM  Internalname: 
+REM  Appicon: Imagotipo.ico
+REM  AdministratorManifest: No
+REM  QBFC Project Options End
+ECHO ON
+start SCRUM.html
