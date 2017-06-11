bd2b757236c461740f162e3d4780783b072b69db
earp519 <earp519@hotmail.com>
2017-06-11 14:47:25 -0500

    actualización junio
    cambios realizados hasta junio 2017

---
+++ psicometricos/motor.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Examenes psicometricos</title>
+	<link rel="stylesheet" href="CSS/normalize.css">
+	<script src="JS/modernizr-custom.js"></script>
+	<link rel="stylesheet" href="CSS/estilo general.css">
+</head>
+<body>
+
+	<header>
+		<figure>
+			<img src="img/TenStep-logo.png" alt="">
+		</figure>
+		<h1>TenStep Latinoamerica</h1>
+	</header>
+	
+	<section class="generales">
+		<form action="">
+			<input type="text" placeholder="Nombre" Name="Nombre" class="text">
+			<input type="text" placeholder="Vacante" Name="Vacante" class="text">
+
+		<label for="hombre">Hombre</label>
+		<input type="radio" name="sexo" value="hombre" id="hombre">
+		<label for="Mujer">Mujer</label>
+		<input type="radio" name="sexo" value="mujer"id="mujer">
+		</form>
+	</section>
+
+	<section>
+		<div class="left">
+				<form class="respuesta">
+				<table>	
+					<tr>
+						<ol>
+							<th>Persuasivo</th>
+							
+							
+							
+						</ol>
+					</tr>
+					<tr>							
+						<th>
+							<th>gentil</th>
+							<input type="text" placeholder="+" Name="+" class="respuesta">
+							<input type="text" placeholder="+" Name="-" class="respuesta">
+							<input type="text" placeholder="+" Name="+" class="respuesta">
+							<input type="text" placeholder="+" Name="-" class="respuesta">
+						</th>
+						<th>
+							<th>humilde</th>
+							<input type="text" placeholder="+" Name="+" class="respuesta">
+							<input type="text" placeholder="+" Name="-" class="respuesta">
+							<input type="text" placeholder="+" Name="+" class="respuesta">
+							<input type="text" placeholder="+" Name="-" class="respuesta">
+						</th>
+						<th>
+							<th>humilde</th>
+							<input type="text" placeholder="+" Name="+" class="respuesta">
+							<input type="text" placeholder="+" Name="-" class="respuesta">
+							<input type="text" placeholder="+" Name="+" class="respuesta">
+							<input type="text" placeholder="+" Name="-" class="respuesta">
+						</th>
+					</tr>
+				</table>
+				</form>
+			</div>
+	</section>
+
+</body>
+</html>
